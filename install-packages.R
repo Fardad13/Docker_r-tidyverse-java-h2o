dad02806ea86f15c77c3f08259e6600b63cd1411
Fardad <fAfscharian@amexus.com>
2019-05-17 18:43:31 +0200

    Initial commit

---
+++ install-packages.R
@@ -0,0 +1,10 @@
+# packages needed for this image ------------------------------------------
+
+install.packages("devtools")
+devtools::install_github("tidyverse/tidyr")
+install.packages("tidyverse")
+install.packages("h2o")
+install.packages("httr")
+install.packages("lubridate")
+install.packages("here")
+install.packages("rJava")
