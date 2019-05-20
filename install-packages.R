41cc0d733a604d8cf3299f1772c05005f84d584c
Fardad <fAfscharian@amexus.com>
2019-05-20 14:50:14 +0200

    changed h2o source from cran to most current aws repo

--- install-packages.R
+++ install-packages.R
@@ -3,7 +3,7 @@
 install.packages("devtools")
 devtools::install_github("tidyverse/tidyr")
 install.packages("tidyverse")
-install.packages("h2o")
+install.packages("h2o", type="source", repos="http://h2o-release.s3.amazonaws.com/h2o/rel-yates/3/R")
 install.packages("httr")
 install.packages("lubridate")
 install.packages("here")
